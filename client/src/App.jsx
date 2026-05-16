8d25ca42e06d46853a9459912d83e6830edd2c6d
Tanisha Ali <tanishaali89@gmail.com>
2026-05-17 01:43:46 +0530

    Installation of vite react, tailwind, framer-motion, lucide-react, clsx, tailwind-merge, react-icons

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './styles/globals.css'
+
+function App() {
+  return (
+    <>
+      <div className='min-h-screen bg-black text-white overflow-x-hidden'>
+        {/* <Navbar /> */}
+      </div>
+    </>
+  )
+}
+
+export default App
